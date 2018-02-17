1fd627e87dd67d8cefd7aacd33d82ce5619da3cc
Sublerti <36573438+Sublerti@users.noreply.github.com>
2018-02-17 23:38:11 +0300

    Loading 1

--- Loading.sh
+++ Loading.sh
@@ -3,32 +3,52 @@ clear
 sleep 0.2
 echo "-"
 sleep 0.2
+clear
 echo "|"
 sleep 0.2
+clear
 echo "-"
 sleep 0.2
+clear
 echo "|"
 sleep 0.2
+clear
 echo "-"
 sleep 0.2
+clear
 echo "|"
 sleep 0.2
+clear
 echo "-"
 sleep 0.2
+clear
 echo "|"
 sleep 0.2
+clear
 echo "-"
 sleep 0.2
+clear
 echo "|"
 sleep 0.2
+clear
 echo "-"
 sleep 0.2
+clear
 echo "|"
 sleep 0.2
+clear
 echo "-"
 sleep 0.2
+clear
 echo "|"
 sleep 0.2
+clear
 echo "-"
 sleep 0.2
+clear
+sleep 0.3 
+echo "done!"
+sleep 0.5
+clear
+echo "Here is a loading script, if you need it, then I authorize you to use it. : 3"
 
